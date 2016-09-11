4362be915783c3357cb88f4f87d9674f2ecacb8a
jmarti <jmarti@theinit.com>
2016-09-11 17:22:26 +0200

    refactor tests

--- src/test/java/com/theinit/tddpractice/carlosble/calculator/CalculatorTest.java
+++ src/test/java/com/theinit/tddpractice/carlosble/calculator/CalculatorTest.java
@@ -3,12 +3,11 @@ package com.theinit.tddpractice.carlosble.calculator;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by INIT SERVICES on 11/9/16.
@@ -19,12 +18,17 @@ public class CalculatorTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    Calculator calculator;
+
+    @Before
+    public void setupCalculator() {
+        this.calculator = new Calculator(-100, 100);
+    }
+
     @Test
     @Parameters({"2, 2, 4",
                  "3, 4, 7" })
     public void addsTwoNumbers(int operand1, int operand2, int expectedResult) throws OverflowException{
-        Calculator calculator = new Calculator(-100, 100);
-
         int result = calculator.add(operand1,operand2);
 
         Assert.assertEquals("The sum of "+operand1+" and "+operand2+" should be "+expectedResult, expectedResult, result);
@@ -34,8 +38,6 @@ public class CalculatorTest {
     @Parameters({"5, 3,  2",
                  "3, 5, -2" })
     public void substractsTwoNumbers(int operand1, int operand2, int expectedResult) throws OverflowException {
-        Calculator calculator = new Calculator(-100, 100);
-
         int result = calculator.substract(operand1,operand2);
 
         Assert.assertEquals("The result of substracting "+operand1+" minus "+operand2+" should be "+expectedResult, expectedResult, result);
@@ -45,7 +47,6 @@ public class CalculatorTest {
     public void addExcedingMaxValue() throws OverflowException {
         thrown.expect(OverflowException.class);
 
-        Calculator calculator = new Calculator(-100, 100);
         calculator.add(50, 150);
     }
 
@@ -53,7 +54,6 @@ public class CalculatorTest {
     public void substractExcedingMinValue() throws OverflowException {
         thrown.expect(OverflowException.class);
 
-        Calculator calculator = new Calculator(-100, 100);
         calculator.substract(10, 150);
     }
 
