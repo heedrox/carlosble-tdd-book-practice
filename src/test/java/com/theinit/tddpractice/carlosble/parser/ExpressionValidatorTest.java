e02360bcf99fdf345c000140e63a8e3db4eec4db
jmarti <jmarti@theinit.com>
2016-09-11 23:52:55 +0200

    add negative cases

--- src/test/java/com/theinit/tddpractice/carlosble/parser/ExpressionValidatorTest.java
+++ src/test/java/com/theinit/tddpractice/carlosble/parser/ExpressionValidatorTest.java
@@ -15,34 +15,37 @@ public class ExpressionValidatorTest {
 
     @Test
     @Parameters({"2 + 3",
-    "253 + 24",
-    "2 * 4",
-    "2 - 3",
-    "2 / 2",
-    "2  +    4",
-    "2-9",
-    "2 + 4 - 3"})
+            "253 + 24",
+            "2 * 4",
+            "2 - 3",
+            "2 / 2",
+            "2  +    4",
+            "2-9",
+            "2 + 4 - 3",
+            "-8 + 2"})
     public void validExpressions(String expression) {
         ExpressionValidator expressionValidator = new ExpressionValidator();
 
         boolean isValid = expressionValidator.isValid(expression);
 
-        assertTrue("valid "+expression, isValid);
+        assertTrue("valid " + expression, isValid);
     }
 
     @Test
     @Parameters({"2 +\\, 3",
             "2a7",
-    "2 -a 4",
-    "2 -+ 3",
-    "+ + 7",
-    "2 + 7 - 2 a 3 b"})
+            "2 -a 4",
+            "2 -+ 3",
+            "+ + 7",
+            "++7",
+            "4++7",
+            "2 + 7 - 2 a 3 b"})
     public void invalidExpressions(String expression) {
         ExpressionValidator expressionValidator = new ExpressionValidator();
 
         boolean isValid = expressionValidator.isValid(expression);
 
-        assertFalse("not valid "+expression, isValid);
+        assertFalse("not valid " + expression, isValid);
     }
 
 }
