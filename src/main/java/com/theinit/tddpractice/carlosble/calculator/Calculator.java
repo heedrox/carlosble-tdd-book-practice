1ef702bf93c0005afdaaaffeea717bcddced3d50
jmarti <jmarti@theinit.com>
2016-09-11 17:02:06 +0200

    add CalculatorTest.substractTwoNumbers and implementation

--- src/main/java/com/theinit/tddpractice/carlosble/calculator/Calculator.java
+++ src/main/java/com/theinit/tddpractice/carlosble/calculator/Calculator.java
@@ -7,4 +7,8 @@ public class Calculator {
     public int add(int operand1, int operand2) {
         return operand1 + operand2;
     }
+
+    public int substract(int operand1, int operand2) {
+        return operand1 - operand2;
+    }
 }
--- src/test/java/com/theinit/tddpractice/carlosble/calculator/CalculatorTest.java
+++ src/test/java/com/theinit/tddpractice/carlosble/calculator/CalculatorTest.java
@@ -15,13 +15,22 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     @Test
-    @Parameters(
-                {"2, 2, 4",
+    @Parameters({"2, 2, 4",
                  "3, 4, 7" })
     public void addsTwoNumbers(int operand1, int operand2, int expectedResult) {
         Calculator calculator = new Calculator();
         int result = calculator.add(operand1,operand2);
         Assert.assertEquals("The sum of "+operand1+" and "+operand2+" should be "+expectedResult, expectedResult, result);
     }
+
+    @Test
+    @Parameters({"5, 3,  2",
+                 "3, 5, -2" })
+    public void substractsTwoNumbers(int operand1, int operand2, int expectedResult) {
+        Calculator calculator = new Calculator();
+        int result = calculator.substract(operand1,operand2);
+        Assert.assertEquals("The result of substracting "+operand1+" minus "+operand2+" should be "+expectedResult, expectedResult, result);
+    }
+
 
 }
