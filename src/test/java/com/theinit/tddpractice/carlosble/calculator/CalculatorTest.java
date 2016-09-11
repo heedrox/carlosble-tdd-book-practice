d445ec2059670960dbba32bcab5bbfebb4f7ba97
jmarti <jmarti@theinit.com>
2016-09-11 16:58:51 +0200

    refactor Calculator.addsTwoNumbers

--- src/test/java/com/theinit/tddpractice/carlosble/calculator/CalculatorTest.java
+++ src/test/java/com/theinit/tddpractice/carlosble/calculator/CalculatorTest.java
@@ -1,26 +1,27 @@
 package com.theinit.tddpractice.carlosble.calculator;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by INIT SERVICES on 11/9/16.
  */
+@RunWith(JUnitParamsRunner.class)
 public class CalculatorTest {
 
     @Test
-    public void addsTwoNumbers() {
+    @Parameters(
+                {"2, 2, 4",
+                 "3, 4, 7" })
+    public void addsTwoNumbers(int operand1, int operand2, int expectedResult) {
         Calculator calculator = new Calculator();
-        int result = calculator.add(2,2);
-        Assert.assertEquals("The sum of 2 and 2 should be 4", 4, result);
+        int result = calculator.add(operand1,operand2);
+        Assert.assertEquals("The sum of "+operand1+" and "+operand2+" should be "+expectedResult, expectedResult, result);
     }
 
-    @Test
-    public void addsAnotherTwoNumbers() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(3,4);
-        Assert.assertEquals("The sum of 2 and 5 should be 7", 7, result);
-    }
 }
