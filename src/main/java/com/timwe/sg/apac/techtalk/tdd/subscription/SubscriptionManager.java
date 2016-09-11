aac66c030bb5455028314df563a32ff84d32dc1a
jmarti <jmarti@theinit.com>
2016-09-11 17:36:48 +0200

    remove old code

--- src/main/java/com/timwe/sg/apac/techtalk/tdd/subscription/SubscriptionManager.java
+++
@@ -1,9 +0,0 @@
-package com.timwe.sg.apac.techtalk.tdd.subscription;
-
-import com.timwe.sg.apac.techtalk.tdd.entity.EntityManager;
-
-/**
- * @author Rashidi Zin
- */
-public interface SubscriptionManager extends EntityManager<Subscription, Long> {
-}
